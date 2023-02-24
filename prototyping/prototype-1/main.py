20004343e5c86064300e9c4eecc574d512e4ba42
Adam Martinez <ajm27@pdx.edu>
2023-02-24 12:27:52 -0800

    updated to using set_duty_cycle and in a loop

--- prototyping/prototype-1/main.py
+++ prototyping/prototype-1/main.py
@@ -7,16 +7,17 @@ BB_FREQ = 250e3
 # Initialize LED to full brightness
 PWM.start("P9_14", 100, BB_FREQ)
 
-# Simulate Transition
+# Simulate Transition Loop
+while True:
 # Gradually adjust the PWM to 0
-for i in range(100, 0, -1):
-    PWM.start("P9_14", i, BB_FREQ)
-    sleep(0.1)
+    for i in range(100, 0, -1):
+        PWM.set_duty_cycle("P9_14", i)
+        sleep(0.1)
 
 # Gradually adjust the PWM to 100
-for i in range(0, 100):
-    PWM.start("P9_14", i, BB_FREQ)
-    sleep(0.1)
+    for i in range(0, 100):
+        PWM.set_duty_cycle("P9_14", i)
+        sleep(0.1)
 
 # Simulate User Set value
-PWM.start("P9_14", 42, BB_FREQ)
+#PWM.set_duty_cycle("P9_14", 42)

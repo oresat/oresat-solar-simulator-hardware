90533e35379be2098a79a8592d860da8efde7415
Adam Martinez <ajm27@pdx.edu>
2023-02-24 10:53:23 -0800

    improved usage of BBIO library

--- prototyping/prototype-1/main.py
+++ prototyping/prototype-1/main.py
@@ -5,18 +5,18 @@ BB_FREQ = 250e3
 # PWM.start(channel, duty, freq=2000, polarity=0)
 
 # Initialize LED to full brightness
-PWM.start("P9_14", 100, freq=BB_FREQ)
+PWM.start("P9_14", 100, BB_FREQ)
 
 # Simulate Transition
 # Gradually adjust the PWM to 0
 for i in range(100, 0, -1):
-    PWM.start("P9_14", i, freq=BB_FREQ)
+    PWM.start("P9_14", i, BB_FREQ)
     sleep(0.1)
 
 # Gradually adjust the PWM to 100
 for i in range(0, 100):
-    PWM.start("P9_14", i, freq=BB_FREQ)
+    PWM.start("P9_14", i, BB_FREQ)
     sleep(0.1)
 
 # Simulate User Set value
-PWM.start("P9_14", 42, freq=BB_FREQ)
+PWM.start("P9_14", 42, BB_FREQ)

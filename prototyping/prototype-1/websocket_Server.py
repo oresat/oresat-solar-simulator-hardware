389a1094a98dda55e5f0f3ab04a4c7cdd146b830
bfaurestal <49964160+bfaurestal@users.noreply.github.com>
2023-03-02 23:51:28 -0800

    update server code

--- prototyping/prototype-1/websocket_Server.py
+++ prototyping/prototype-1/websocket_Server.py
@@ -1,12 +1,57 @@
-import asyncio
-import websockets
+import os
+import sys
+import tornado.ioloop
+from tornado.options import define, options, parse_command_line
+import tornado.web
 
-# create handler for each connection
-async def handler(websocket, path):
-    data = await websocket.recv()
-    reply = f"Data recieved as:  {data}!"
-    await websocket.send(reply)
-start_server = websockets.serve(handler, "localhost", 8090)
+import socketio
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+define("port", default=8080, help="run on the given port", type=int)
+define("debug", default=False, help="run in debug mode")
+
+msg1 ="ON"
+msg2 ="OFF"
+
+sio = socketio.AsyncServer(async_mode='tornado')
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("latency.html")
+
+
+@sio.event
+async def ping_from_client(sid):
+    await sio.emit('pong_from_server', room=sid)
+    #print ('pong from server')
+    #print (sid)
+
+@sio.event
+def command(sid, data):
+    print('message',data);
+
+@sio.event
+def disconnect(sid):
+        print('disconnect ', sid)
+
+
+def main():
+    parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            (r"/socket.io/", socketio.get_tornado_handler(sio)),
+        ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        debug=options.debug,
+    )
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    print(f"Arg counts: {len(sys.argv)}")
+    mes = sys.argv
+    print(f"Arg is: {mes}")
+    main()

b9641089ca42437801e7fa4aeea3a3afcbfa9139
Bernadeau <49964160+bfaurestal@users.noreply.github.com>
2023-03-07 20:43:37 -0800

    Update websocket_Server.py

--- prototyping/prototype-1/websocket_Server.py
+++ prototyping/prototype-1/websocket_Server.py
@@ -1,57 +1,30 @@
 import os
 import sys
-import tornado.ioloop
-from tornado.options import define, options, parse_command_line
-import tornado.web
-
 import socketio
+import eventlet
 
-define("port", default=8080, help="run on the given port", type=int)
-define("debug", default=True, help="run in debug mode")
+local_address='192.168.50.48' 
+port = 8080
 
-msg1 ="ON"
-msg2 ="OFF"
-
-sio = socketio.AsyncServer(async_mode='tornado')
-
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("latency.html")
-
+sio = socketio.Server(logger=False,async_mode='eventlet')
+app = socketio.WSGIApp(sio)
 
 @sio.event
-async def ping_from_client(sid):
-    await sio.emit('pong_from_server', room=sid)
-    #print ('pong from server')
-    #print (sid)
-
+def connect(sid, environ, auth):
+    
+    print(f"Arg counts: {len(sys.argv)}")
+    mes = sys.argv[1]
+    print(f"Arg is: {mes}")
+    print('connect ', sid)
+    sio.emit('cmd', mes)
+   
 @sio.event
-def command(sid, data):
-    print('message',data);
+def my_message(sid, data):
+    print('message ', data)
 
 @sio.event
 def disconnect(sid):
-        print('disconnect ', sid)
+    print('disconnect ', sid)
 
-
-def main():
-    parse_command_line()
-    app = tornado.web.Application(
-        [
-            (r"/", MainHandler),
-            (r"/socket.io/", socketio.get_tornado_handler(sio)),
-        ],
-        template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path=os.path.join(os.path.dirname(__file__), "static"),
-        debug=options.debug,
-    )
-    app.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
-
-
-if __name__ == "__main__":
-    print(f"Arg counts: {len(sys.argv)}")
-    mes = sys.argv
-    print(f"Arg is: {mes}")
-    main()
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen((local_address, port)), app)

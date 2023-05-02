553d4bf7a169234dd854fd5e75bb0b52ff397834
Adam Martinez <ajm27@pdx.edu>
2023-05-02 14:15:06 -0700

    updated values for SPI

--- prototyping/prototype-2/p2_validation.py
+++ prototyping/prototype-2/p2_validation.py
@@ -7,16 +7,16 @@ from time import sleep
 
 # I2C
 # Red LED - DAC Channel A
-# Red LED - DAC Channel B
-# Red LED - DAC Channel C
-# Red LED - DAC Channel D
+# Grn LED - DAC Channel B
+# Blu LED - DAC Channel C
+# UV  LED - DAC Channel D
 
 # SPI
 # 8 bit address 0-2 & 6-7 are DC
 # Therm1  - ADC IN0 - 000 - 0
-# Therm2  - ADC IN1 - 001 - 8
-# Therm3  - ADC IN2 - 010 - 16
-# PhotoD  - ADC IN3 - 011 - 24
+# Therm2  - ADC IN1 - 001 - 1
+# Therm3  - ADC IN2 - 010 - 2
+# PhotoD  - ADC IN3 - 011 - 3
 
 running = True
 max_voltage = 65535
@@ -41,14 +41,14 @@ while running:
 
     # SPI Read every 5th set
     if i % 5 == 0:
-        spi.writebytes(0)
-        print(f"Therm1: {spi.readbytes(8)}")
-        spi.writebytes(8)
-        print(f"Therm2: {spi.readbytes(8)}")
-        spi.writebytes(16)
-        print(f"Therm3: {spi.readbytes(8)}")
-        spi.writebytes(24)
-        print(f"Photo: {spi.readbytes(8)}")
+        spi.writebytes([0])
+        print(f"Therm1: {spi.readbytes(2)}")
+        spi.writebytes([1])
+        print(f"Therm2: {spi.readbytes(2)}")
+        spi.writebytes([2])
+        print(f"Therm3: {spi.readbytes(2)}")
+        spi.writebytes([3])
+        print(f"Photo: {spi.readbytes(2)}")
     print(i)
     i += direction
     sleep(0.1)

766d5b973f967532a3d0005c941c6b4e8bd03188
Adam Martinez <ajm27@pdx.edu>
2023-05-05 11:23:22 -0700

    i2c control working

--- prototyping/prototype-2/p2_validation.py
+++ prototyping/prototype-2/p2_validation.py
@@ -2,8 +2,9 @@ import board
 import busio
 import adafruit_mcp4728
 from Adafruit_BBIO.SPI import SPI
-from numpy import linspace, int16
+from numpy import linspace, uint16
 from time import sleep
+from smbus2 import SMBus
 
 # I2C
 # Red LED - DAC Channel A
@@ -20,37 +21,40 @@ from time import sleep
 
 running = True
 max_voltage = 65535
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C('I2C1_SCL', 'I2C1_SDA')
+# i2c = board.I2C()
 mcp4728 = adafruit_mcp4728.MCP4728(i2c)
+# print(board.SDA2)
 spi = SPI(0, 0)	 # /dev/spidev1.0
-steps = linspace(0, max_voltage, num=100, dtype=int16)
+steps = linspace(0, max_voltage, num=100, dtype=uint16)
 direction = 1
-i = 0
+i = 1
 while running:
+    print(steps[i])
     # Red LED
-    mcp4728.channel_a.value = max_voltage
+    mcp4728.channel_a.value = steps[i]
 
     # Green LED
-    mcp4728.channel_b.value = max_voltage
+    mcp4728.channel_b.value = steps[i]
 
     # Blue LED
-    mcp4728.channel_c.value = max_voltage
+    mcp4728.channel_c.value = steps[i]
 
-    # UV LED
-    mcp4728.channel_d.value = max_voltage
+    # # UV LED
+    # mcp4728.channel_d.value = steps[i]
 
     # SPI Read every 5th set
-    if i % 5 == 0:
-        spi.writebytes([0])
-        print(f"Therm1: {spi.readbytes(2)}")
-        spi.writebytes([1])
-        print(f"Therm2: {spi.readbytes(2)}")
-        spi.writebytes([2])
-        print(f"Therm3: {spi.readbytes(2)}")
-        spi.writebytes([3])
-        print(f"Photo: {spi.readbytes(2)}")
-    print(i)
+    # if i % 5 == 0:
+    #     spi.writebytes([0])
+    #     print(f"Therm1: {spi.readbytes(2)}")
+    #     spi.writebytes([1])
+    #     print(f"Therm2: {spi.readbytes(2)}")
+    #     spi.writebytes([2])
+    #     print(f"Therm3: {spi.readbytes(2)}")
+    #     spi.writebytes([3])
+    #     print(f"Photo: {spi.readbytes(2)}")
+    # print(f'{i}: {steps[i]}')
     i += direction
     sleep(0.1)
-    if i >= 100 or i <= 0:
+    if i >= 99 or i <= 0:
         direction *= -1
